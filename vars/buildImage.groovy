bf883cc5a7e4f272d37437d2541b17ed0c880755
muhammedelgendy97 <61365303+muhammedelgendy97@users.noreply.github.com>
2023-02-02 20:20:24 +0200

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
-def call(){
+def call(String imageName){
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t mohamedelgendy/my-repo:jma-2.1 .'
+        sh 'docker build -t $imageName .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push mohamedelgendy/my-repo:jma-2.1'
+        sh 'docker push $imageName'
     }
 } 
